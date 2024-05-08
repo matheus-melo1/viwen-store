c8ecc276580325775f846ec62454cbd41d2b88ce
shui <matheusmelomr@hotmail.com>
2024-05-08 14:30:25 -0300

    修正された 6

--- js/routes.js
+++ js/routes.js
@@ -10,7 +10,11 @@ const initApp = () => {
   let thisProduct = data.product.filter(product => product.id == productID)[0];
   if (!thisProduct) window.location.href = '/';
 
-  document.querySelector('.text').innerHTML = `${thisProduct.name}`;
-  document.querySelector('.img').src = `${thisProduct.img}`;
+  document.querySelector('title').innerHTML = `Viwen | ${thisProduct.name}`;
+  document.querySelector('.sell-title').innerHTML = `${thisProduct.name}`;
+  document.querySelector('.sell-price').innerHTML = `R$ ${thisProduct.price}`;
+  document.querySelector('.sell-portion').innerHTML = `${thisProduct.portion}x de R$${thisProduct.portionPrice} sem juros`;
+  document.querySelector('.card-category').innerHTML = `${thisProduct.category}`;
+  document.querySelector('.sell-img img').src = `${thisProduct.img}`;
 }
 initApp();
